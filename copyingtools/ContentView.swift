041a2f08685894b1c154d1e89b71dc5396b926a9
bin.sun <bin.sun@fh.com>
2025-03-14 14:00:26 +0800

    【功能】初始化项目

---
+++ copyingtools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  copyingtools
+//
+//  Created by pizzaman on 2025/3/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ copyingtools/copyingtoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  copyingtoolsApp.swift
+//  copyingtools
+//
+//  Created by pizzaman on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct copyingtoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
